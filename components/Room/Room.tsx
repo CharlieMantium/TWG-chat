394594075afaf1711d883d3c9bd152e4c5506031
CharlieMantium <karol.zieniewicz@gmail.com>
2020-10-22 17:31:04 +0200

    added message input to rooms

--- components/Room/Room.tsx
+++ components/Room/Room.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
-import { View, Button, Text } from 'react-native';
+import {
+  View,
+  Button,
+  Text,
+  TextInput,
+  NativeSyntheticEvent,
+  TextInputChangeEventData
+} from 'react-native';
 import { useQuery } from '@apollo/client';
 
 import { RoomScreenNavigationProp } from '../../types/types';
@@ -14,6 +21,13 @@ const RoomWrapper = styled(View)`
   border: 1px solid ${colors.beta};
 `;
 
+const MessageInputWrapper = styled(View)``;
+
+const MessageInput = styled(TextInput)`
+  height: 40px;
+  border: 1px solid black;
+`;
+
 interface RoomProps {
   navigation: RoomScreenNavigationProp;
   route: RoomScreenRouteProp;
@@ -21,18 +35,26 @@ interface RoomProps {
 
 const Room: React.FC<RoomProps> = ({ route, navigation }) => {
   const { name, id } = route.params;
+  const [ message, setMessage ] = useState('');
   const { loading, data } = useQuery(GET_MESSAGES, {
     variables: { room: id },
   });
+
+  const onMessageChange = (e: NativeSyntheticEvent<TextInputChangeEventData>): void => 
+    {setMessage(e.nativeEvent.text)};
 
   return (
     <RoomWrapper>
       <Text>{name} : {id}</Text>
       <Button title="Go to selection" onPress={() => navigation.navigate('Selection')}/>
       {loading && (<Text>Loading...</Text>)}
-      {data && data.room.messages.reverse().map(({body, id}: {body: string, id: string}) => (
+      {data && data.room.messages.map(({body, id}: {body: string, id: string}) => (
         <Text key={id}>{body}</Text>
       ))}
+      <MessageInputWrapper>
+        <MessageInput value={message} onChange={onMessageChange}/>
+        <Button title="Send" onPress={() => {console.log(message)}} />
+      </MessageInputWrapper>
     </RoomWrapper>
     );
 };
